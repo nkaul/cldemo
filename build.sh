d525d746e5244795590fe8de237564d86bf8edb7
natm <nat@nuqe.net>
2014-03-31 15:25:04 -0700

    improved build script, fixed autosign.conf issue

--- build.sh
+++ build.sh
@@ -2,22 +2,30 @@
 
 # clean up
 rm -rf repo/*
-mkdir -p repo/dists/cldemo/workbench/binary-amd64
-mkdir -p repo/dists/cldemo/workbench/binary-i386
-mkdir -p repo/dists/cldemo/switch/binary-amd64
-mkdir -p repo/dists/cldemo/switch/binary-i386
 
-# build debs
-for D in `find pkgs/workbench/* -maxdepth 0 -type d`
+# loop through repos
+for R in `find pkgs/* -maxdepth 0 -type d`
 do
-    PKG=$(echo $D | sed -e "s/pkgs\/workbench\///")
-    echo $PKG
-    dpkg-deb --build pkgs/workbench/$PKG/debian repo/dists/cldemo/workbench/binary-amd64/${PKG}_amd64.deb
-done
 
-# create repo
-dpkg-scanpackages -a amd64 repo/dists/cldemo/workbench/binary-amd64 /dev/null | sed -e 's/Filename: repo\//Filename: /' > repo/dists/cldemo/workbench/binary-amd64/Packages
-dpkg-scanpackages -a i386 repo/dists/cldemo/workbench/binary-amd64 /dev/null | sed -e 's/Filename: repo\//Filename: /' > repo/dists/cldemo/workbench/binary-i386/Packages
+    REPO=$(echo $R | sed -e "s/pkgs\///")
+
+    # create empty repos
+    mkdir -p repo/dists/cldemo/$REPO/binary-amd64
+    mkdir -p repo/dists/cldemo/$REPO/binary-i386
+
+    # loop through packages
+    for P in `find pkgs/$REPO/* -maxdepth 0 -type d`
+    do
+        PKG=$(echo $P | sed -e "s/pkgs\/$REPO\///")
+        dpkg-deb --build pkgs/$REPO/$PKG/debian repo/dists/cldemo/$REPO/binary-amd64/${PKG}_amd64.deb
+    done
+
+    # generate package lists
+    dpkg-scanpackages -a amd64 repo/dists/cldemo/$REPO/binary-amd64 /dev/null | sed -e 's/Filename: repo\//Filename: /' > repo/dists/cldemo/$REPO/binary-amd64/Packages
+    dpkg-scanpackages -a i386 repo/dists/cldemo/$REPO/binary-amd64 /dev/null | sed -e 's/Filename: repo\//Filename: /' > repo/dists/cldemo/$REPO/binary-i386/Packages
+
+
+done
 
 # upload
 scp -r repo nat@dev.cumulusnetworks.com:public_html/cldemo/
