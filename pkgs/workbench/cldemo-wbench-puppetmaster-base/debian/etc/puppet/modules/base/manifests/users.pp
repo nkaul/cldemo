97195d70a7557c0e60e77bb7074488863f8ca791
Leslie Carr <geekgirl@gmail.com>
2014-04-22 13:50:09 -0700

    using git submodule for user accounts

--- pkgs/workbench/cldemo-wbench-puppetmaster-base/debian/etc/puppet/modules/base/manifests/users.pp
+++ pkgs/workbench/cldemo-wbench-puppetmaster-base/debian/etc/puppet/modules/base/manifests/users.pp
@@ -1,20 +1,17 @@
 class base::users {
 
-  base::useraccount { 'rocket':
-    $username = 'rocket'
-    $uid = '600'
-    $gid = ['cumulus', 'sudo']
-    $sshkey = 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCs0iscDeejPmguBLIKoes6bZrxIq89mW4dfeox9Bhel1qqDVJZjeqgj6MaI53WnNHcLZFzNyIL4wzNvTL9dVIgc3hHissiieFvtK71q9xqbxwYbySq+yKxJMTe+MrbyLpj5XZVDL/E8/xVxP+YIXMpHm9GeMVeyAT/fPRgkXHGWoHsBz7bGJQi6s78NU0gjtIuT2h+mKhSo2ZZrDNjqR11x5AVjZLB304Y3UxNCsuTjozNNu5JqjW2+9QhKWHLI3kUWUz9EjCV77KKS0GbevQo7Mx8D7uQT3IQxbm3UBhK88GnZeRzkP4ULZ5uYkY1D3hiuSAirDBTdT2lHc/iTX7R'
-    $password = ''
+  account { 'rocket':
+    uid     => '600'
+    groups  => ['cumulus', 'sudo']
+    ssh_key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCs0iscDeejPmguBLIKoes6bZrxIq89mW4dfeox9Bhel1qqDVJZjeqgj6MaI53WnNHcLZFzNyIL4wzNvTL9dVIgc3hHissiieFvtK71q9xqbxwYbySq+yKxJMTe+MrbyLpj5XZVDL/E8/xVxP+YIXMpHm9GeMVeyAT/fPRgkXHGWoHsBz7bGJQi6s78NU0gjtIuT2h+mKhSo2ZZrDNjqR11x5AVjZLB304Y3UxNCsuTjozNNu5JqjW2+9QhKWHLI3kUWUz9EjCV77KKS0GbevQo7Mx8D7uQT3IQxbm3UBhK88GnZeRzkP4ULZ5uYkY1D3hiuSAirDBTdT2lHc/iTX7R'
   }
 
 
-  base::useraccount { 'turtle':
-    $username = 'turtle'
-    $uid = '601'
-    $gid = ['cumulus', 'sudo']
+  account { 'turtle':
+    uid      => '601',
+    gid      => ['cumulus', 'sudo'],
     #password is lettuce
-    $password = '$1$mKzk5zJc$rjQIr24v.ZF27J7TAaZxQ.'
+    password => '$1$mKzk5zJc$rjQIr24v.ZF27J7TAaZxQ.'
   }
 
   base::sudo_user { ['rocket', 'turtle']:
