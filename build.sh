79efa2d236e16335ba90260a8ab9813dcffc5d83
natm <nat@nuqe.net>
2014-04-22 03:55:01 -0700

    changed package output dir to repo-build/, new repo-static folder for scripts etc

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 # clean up
-rm -rf repo/*
+if [ ! -e repo-build ]; then
+    mkdir repo-build
+else
+    rm -rf repo-build/*
+fi
 
 # loop through repos
 for R in `find pkgs/* -maxdepth 0 -type d`
@@ -10,22 +14,23 @@ do
     REPO=$(echo $R | sed -e "s/pkgs\///")
 
     # create empty repos
-    mkdir -p repo/dists/cldemo/$REPO/binary-amd64
-    mkdir -p repo/dists/cldemo/$REPO/binary-i386
-    mkdir -p repo/dists/cldemo/$REPO/binary-powerpc
+    mkdir -p repo-build/dists/cldemo/$REPO/binary-amd64
+    mkdir -p repo-build/dists/cldemo/$REPO/binary-i386
+    mkdir -p repo-build/dists/cldemo/$REPO/binary-powerpc
 
     # loop through packages
     for P in `find pkgs/$REPO/* -maxdepth 0 -type d`
     do
         PKG=$(echo $P | sed -e "s/pkgs\/$REPO\///")
-        dpkg-deb --build pkgs/$REPO/$PKG/debian repo/dists/cldemo/$REPO/binary-amd64/${PKG}_amd64.deb
+        dpkg-deb --build pkgs/$REPO/$PKG/debian repo-build/dists/cldemo/$REPO/binary-amd64/${PKG}_amd64.deb
     done
 
     # generate package lists
-    dpkg-scanpackages -a amd64 repo/dists/cldemo/$REPO/binary-amd64 /dev/null | sed -e 's/Filename: repo\//Filename: /' > repo/dists/cldemo/$REPO/binary-amd64/Packages
-    dpkg-scanpackages -a i386 repo/dists/cldemo/$REPO/binary-amd64 /dev/null | sed -e 's/Filename: repo\//Filename: /' > repo/dists/cldemo/$REPO/binary-i386/Packages
-    dpkg-scanpackages -a powerpc repo/dists/cldemo/$REPO/binary-powerpc /dev/null | sed -e 's/Filename: repo\//Filename: /' > repo/dists/cldemo/$REPO/binary-powerpc/Packages
-
+    dpkg-scanpackages -a amd64 repo-build/dists/cldemo/$REPO/binary-amd64 /dev/null | sed -e 's/Filename: repo-build\//Filename: /' > repo-build/dists/cldemo/$REPO/binary-amd64/Packages
+    dpkg-scanpackages -a i386 repo-build/dists/cldemo/$REPO/binary-amd64 /dev/null | sed -e 's/Filename: repo-build\//Filename: /' > repo-build/dists/cldemo/$REPO/binary-i386/Packages
+    dpkg-scanpackages -a powerpc repo-build/dists/cldemo/$REPO/binary-powerpc /dev/null | sed -e 's/Filename: repo-build\//Filename: /' > repo-build/dists/cldemo/$REPO/binary-powerpc/Packages
 
 done
 
+# copy static content
+cp -R repo-static/* repo-build/
