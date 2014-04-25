5150c47e2c1f09235eac6f3a824f09d7f51fca48
Leslie Carr <geekgirl@gmail.com>
2014-04-24 17:33:52 -0700

    early commit of content

---
+++ pkgs/workbench/cldemo-wbench-ganglia-puppet/debian/etc/puppet/modules/monitoring/manifests/webhost.pp
@@ -0,0 +1,29 @@
+class monitoring::webhost {
+  package { 'apache2':
+    ensure => installed
+  }
+  
+  service { 'apache2':
+    ensure  => running,
+    enabled => true,
+    require Package['apache2'],
+  }
+  
+  file { '/etc/apache2/sites-available/ganglia.lab.local':
+    ensure => present,
+    owner  => 'www-data',
+    group  => 'www-data',
+    mode   => '0644',
+    notify => Service['apache2'],
+  }
+
+  file { '/etc/apache2/sites-enabled/ganglia.lab.local':
+    ensure  => link,
+    require => File['/etc/apache2/sites-available/ganglia.lab.local'],
+  }
+
+  package { [ 'ganglia-monitor', 'gmetad', 'ganglia-webfrontend' ]: 
+    ensure => installed
+  }
+
+}
