73ec1f3c92f4a855b75b6b5d80ece6e1ed0e38c0
natm <nat@nuqe.net>
2014-04-25 07:08:50 -0700

    build script stops upon pkg failure

--- build.sh
+++ build.sh
@@ -10,30 +10,51 @@ fi
 # sync git submodules
 git submodule init
 git submodule update
+
+architectures=(amd64 i386 powerpc)
+
 # loop through repos
 for R in `find pkgs/* -maxdepth 0 -type d`
 do
 
     REPO=$(echo $R | sed -e "s/pkgs\///")
 
+    echo ""
+    echo "$REPO repo..."
+    echo ""
+
     # create empty repos
-    mkdir -p repo-build/dists/cldemo/$REPO/binary-amd64
-    mkdir -p repo-build/dists/cldemo/$REPO/binary-i386
-    mkdir -p repo-build/dists/cldemo/$REPO/binary-powerpc
+    for ARCH in ${architectures[@]}; do
+        mkdir -p repo-build/dists/cldemo/$REPO/binary-$ARCH
+    done
 
     # loop through packages
     for P in `find pkgs/$REPO/* -maxdepth 0 -type d`
     do
         PKG=$(echo $P | sed -e "s/pkgs\/$REPO\///")
-        dpkg-deb --build pkgs/$REPO/$PKG/debian repo-build/dists/cldemo/$REPO/binary-amd64/${PKG}_amd64.deb
+        if ! dpkg-deb --build pkgs/$REPO/$PKG/debian repo-build/dists/cldemo/$REPO/binary-amd64/${PKG}_amd64.deb
+	then
+	    echo "** ERROR ** while building $REPO/$PKG"
+	    exit 1
+	fi
     done
 
     # generate package lists
-    dpkg-scanpackages -a amd64 repo-build/dists/cldemo/$REPO/binary-amd64 /dev/null | sed -e 's/Filename: repo-build\//Filename: /' > repo-build/dists/cldemo/$REPO/binary-amd64/Packages
-    dpkg-scanpackages -a i386 repo-build/dists/cldemo/$REPO/binary-amd64 /dev/null | sed -e 's/Filename: repo-build\//Filename: /' > repo-build/dists/cldemo/$REPO/binary-i386/Packages
-    dpkg-scanpackages -a powerpc repo-build/dists/cldemo/$REPO/binary-powerpc /dev/null | sed -e 's/Filename: repo-build\//Filename: /' > repo-build/dists/cldemo/$REPO/binary-powerpc/Packages
+    for ARCH in ${architectures[@]}; do
+        if ! dpkg-scanpackages -a $ARCH repo-build/dists/cldemo/$REPO/binary-$ARCH /dev/null | sed -e 's/Filename: repo-build\//Filename: /' > repo-build/dists/cldemo/$REPO/binary-$ARCH/Packages
+        then
+            echo "** ERROR ** while generating repo for  $REPO $ARCH"
+            exit 1
+        fi
+    done
 
 done
 
 # copy static content
 cp -R repo-static/* repo-build/
+
+echo ""
+echo "Done"
+echo ""
+
+exit 0
