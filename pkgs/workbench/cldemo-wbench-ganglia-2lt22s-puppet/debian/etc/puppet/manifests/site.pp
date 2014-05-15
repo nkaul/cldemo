17d0915bc9bb037666db21970254c639723bc42f
Leslie Carr <geekgirl@gmail.com>
2014-05-15 16:46:12 -0700

    fixed switch class name

--- pkgs/workbench/cldemo-wbench-ganglia-2lt22s-puppet/debian/etc/puppet/manifests/site.pp
+++ pkgs/workbench/cldemo-wbench-ganglia-2lt22s-puppet/debian/etc/puppet/manifests/site.pp
@@ -4,7 +4,7 @@ node 'spine1.lab.local' {
     $int_loopback = '10.2.1.3'
     $int_unnumbered = [ 'swp49', 'swp50', 'swp51', 'swp52' ]
     $int_bridges = { }
-    include monitoring::role::switchbase
+    include monitoring::role
 }
 
 node 'spine2.lab.local' {
@@ -12,7 +12,7 @@ node 'spine2.lab.local' {
     $int_loopback = '10.2.1.4'
     $int_unnumbered = [ 'swp49', 'swp50', 'swp51', 'swp52' ]
     $int_bridges = { }
-    include monitoring::role::switchbase
+    include monitoring::role
 }
 
 node 'leaf1.lab.local' {
@@ -23,7 +23,7 @@ node 'leaf1.lab.local' {
         br0 => { 'address' => '10.4.1.1', 'netmask' => '255.255.255.128', 'members' => ['swp32s0'] },
         br1 => { 'address' => '10.4.1.129', 'netmask' => '255.255.255.128', 'members' => ['swp32s1'] }
     }
-    include monitoring::role::switchbase
+    include monitoring::role
 }
 
 node 'leaf2.lab.local' {
@@ -34,7 +34,7 @@ node 'leaf2.lab.local' {
         br0 => { 'address' => '10.4.2.1', 'netmask' => '255.255.255.128', 'members' => ['swp30','swp31','swp32','swp33'] },
         br1 => { 'address' => '10.4.2.129', 'netmask' => '255.255.255.128', 'members' => ['swp34','swp35','swp36','swp37'] }
     }
-    include monitoring::role::switchbase
+    include monitoring::role
 }
 
 node 'wbench.lab.local' {
