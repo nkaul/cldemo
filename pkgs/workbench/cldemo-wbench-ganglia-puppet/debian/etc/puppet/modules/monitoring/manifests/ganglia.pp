423aadcc5fd820fc15a76f57de7a09b10e58aa9a
Leslie Carr <geekgirl@gmail.com>
2014-05-27 14:37:56 -0700

    adding multi interface module to ganglia

--- pkgs/workbench/cldemo-wbench-ganglia-puppet/debian/etc/puppet/modules/monitoring/manifests/ganglia.pp
+++ pkgs/workbench/cldemo-wbench-ganglia-puppet/debian/etc/puppet/modules/monitoring/manifests/ganglia.pp
@@ -21,4 +21,39 @@ class monitoring::ganglia {
     notify => Service['gmond']
   }
 
+  # include multiple interface module
+  file { '/usr/lib/ganglia/python_modules/':
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+  }
+
+  file { '/usr/lib/ganglia/python_modules/multi_interface.py':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    require => File['/usr/lib/ganglia/python_modules/'],
+    source  => 'puppet:///modules/monitoring/multi_interface.py',
+    notify  => Service['gmond']
+  }
+
+  file { '/etc/ganglia/conf.d':
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+  }
+
+  file { '/etc/ganglia/conf.d/multi_interface.pyconf':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    require => File['/etc/ganglia/conf.d'],
+    source  => 'puppet:///modules/monitoring/multi_interface.pyconf',
+    notify  => Service['gmond']
+  }
+
 }
