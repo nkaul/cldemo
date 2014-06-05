3d4f72b64028ba34eb28659da8e42c42125debc8
Leslie Carr <geekgirl@gmail.com>
2014-06-05 16:17:11 -0700

    forked a 2switch version of the ganglia lab

---
+++ pkgs/workbench/cldemo-wbench-ganglia-2s-puppet/debian/etc/puppet/manifests/site.pp
@@ -0,0 +1,29 @@
+node "leaf1.lab.local" {
+    $int_enabled = true
+    $int_loopback = "10.2.1.1"
+    $int_unnumbered = [ "swp1", "swp2", "swp3", "swp4" ]
+    $int_bridges = {
+        br0 => { 'address' => '10.4.1.1', 'netmask' => '255.255.255.128', 'members' => ['swp30','swp31','swp32','swp33'] },
+        br1 => { 'address' => '10.4.1.129', 'netmask' => '255.255.255.128', 'members' => ['swp34','swp35','swp36','swp37'] }
+    }
+    include monitoring::role::switch
+}
+ 
+node "leaf2.lab.local" {
+    $int_enabled = true
+    $int_loopback = "10.2.1.2"
+    $int_unnumbered = [ "swp1", "swp2", "swp3", "swp4" ]
+    $int_bridges = {
+        br0 => { 'address' => '10.4.2.1', 'netmask' => '255.255.255.128', 'members' => ['swp30','swp31','swp32','swp33'] },
+        br1 => { 'address' => '10.4.2.129', 'netmask' => '255.255.255.128', 'members' => ['swp34','swp35','swp36','swp37'] }
+    }
+    include monitoring::role::switch
+}
+
+node 'wbench.lab.local' {
+    $data_sources = [ 'wbench.lab.local' ]
+    $websites = [
+      { title => 'Ganglia', location => 'ganglia/' }
+    ]
+    include monitoring::role::wbench
+}
