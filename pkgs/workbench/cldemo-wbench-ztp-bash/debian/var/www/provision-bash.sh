9c7635b17303db3bb69eeb64bda6bea401a6026c
Leslie Carr <geekgirl@gmail.com>
2014-06-11 15:35:32 -0700

    new module - bash only ZTP provisioning

---
+++ pkgs/workbench/cldemo-wbench-ztp-bash/debian/var/www/provision-bash.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -x
+echo "Adding users" | wall -n
+#adding users rocket and turtle with password lettuce
+useradd rocket -g cumulus -G sudo -m -s /bin/bash -d /home/rocket -p '$1$rA6KCeAS$QBfUBCwU19cueiECo3pOg1'
+useradd turtle -g cumulus -G sudo -m -s /bin/bash -d /home/turtle -p '$1$rA6KCeAS$QBfUBCwU19cueiECo3pOg1'
+
+echo "Updating MOTD" | wall -n
+
+echo "`hostname` configured by bash ZTP" > /etc/motd
+# CUMULUS-AUTOPROVISIONING
+
+exit 0
