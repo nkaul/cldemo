3dc19732a0955138624021545d0586bf77c86ab1
Leslie Carr <geekgirl@gmail.com>
2014-06-23 16:33:21 -0700

    fixing postinst typo

--- pkgs/workbench/cldemo-wbench-base-puppet/debian/etc/puppet/modules/monitoring/manifests/hsflowd.pp
+++ pkgs/workbench/cldemo-wbench-base-puppet/debian/etc/puppet/modules/monitoring/manifests/hsflowd.pp
@@ -1,7 +1,5 @@
 class monitoring::hsflowd {
   # Warning: this class will not properly work until 2.1
-  # You must manually install hsflowd from latest source and
-  # use a working beta switchd.deb
 
   package hsflowd {
      ensure => installed;
