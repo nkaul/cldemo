2cdd07c214198868a34a53df4f7f2c0c3a4b0fac
natm <nat@nuqe.net>
2014-07-09 15:47:48 -0700

    CFEngine ZTP autoprovisioning script

--- pkgs/workbench/cldemo-wbench-ztp-cfengine/debian/var/www/provision-cfengine.sh
+++ pkgs/workbench/cldemo-wbench-ztp-cfengine/debian/var/www/provision-cfengine.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Add CFEngine repository
+wget -qO- https://s3.amazonaws.com/cfengine.package-repos/pub/gpg.key | apt-key add -
+echo "deb http://cfengine.com/pub/apt/packages stable main" > /etc/apt/sources.list.d/cfengine-community.list
+
+# install cfengine
+apt-get update -y
+apt-get upgrade -y
+apt-get install cfengine-community -y
 
 #CUMULUS-AUTOPROVISIONING
 
