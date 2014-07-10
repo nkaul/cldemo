f4c05ba98e9753d34cb116c4b0710a3fc6d5c989
natm <nat@nuqe.net>
2014-07-10 11:35:52 -0700

    removing puppetlabs repo, both x86 and ppc pkgs in cumulus repo

--- pkgs/workbench/cldemo-wbench-ztp-puppet/debian/var/www/provision-puppet.sh
+++ pkgs/workbench/cldemo-wbench-ztp-puppet/debian/var/www/provision-puppet.sh
@@ -3,11 +3,6 @@
 # Allow Cumulus testing repo
 sed -i /etc/apt/sources.list -e 's/# deb http:\/\/repo.cumulusnetworks.com CumulusLinux-2.1 testing/deb http:\/\/repo.cumulusnetworks.com CumulusLinux-2.1 testing/'
 
-# Add PuppetLabs repo
-wget -o /tmp/puppetlabs-pubkey.gpg http://apt.puppetlabs.com/pubkey.gpg
-gpg --import /tmp/puppetlabs-pubkey.gpg
-gpg -a --export 4BD6EC30 | apt-key add -
-
 # Upgrade and install Puppet
 apt-get update -y
 apt-get upgrade -y
