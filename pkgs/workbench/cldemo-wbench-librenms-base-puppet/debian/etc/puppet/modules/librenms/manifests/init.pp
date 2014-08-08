e6cca4b67a4efb512b8a2bec38bdb0184b75d6bc
Leslie Carr <geekgirl@gmail.com>
2014-08-08 12:11:54 -0700

    fix ae2enmod command

--- pkgs/workbench/cldemo-wbench-librenms-base-puppet/debian/etc/puppet/modules/librenms/manifests/init.pp
+++ pkgs/workbench/cldemo-wbench-librenms-base-puppet/debian/etc/puppet/modules/librenms/manifests/init.pp
@@ -116,7 +116,7 @@ class librenms(
         ensure => present,
     }
 
-    exec { "a2enmon rewrite":
+    exec { "a2enmod rewrite":
       refreshonly => true,
       subscribe   => Package['apache2'],
       require     => Package['apache2'],
