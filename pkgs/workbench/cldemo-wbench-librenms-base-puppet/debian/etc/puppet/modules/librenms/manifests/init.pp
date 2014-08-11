160e4f12ba3b9a8055ee6e797747c5a7d7b29946
Leslie Carr <geekgirl@gmail.com>
2014-08-11 14:43:27 -0700

    updating one-time-only commands

--- pkgs/workbench/cldemo-wbench-librenms-base-puppet/debian/etc/puppet/modules/librenms/manifests/init.pp
+++ pkgs/workbench/cldemo-wbench-librenms-base-puppet/debian/etc/puppet/modules/librenms/manifests/init.pp
@@ -117,16 +117,20 @@ class librenms(
         ensure => present,
     }
 
+    exec { '/usr/bin/touch /tmp/puppet_once_lock':
+      creates => '/tmp/puppet_once_lock',
+      notify  => [Exec['/usr/sbin/a2enmod rewrite'],Exec['/usr/bin/php /var/www/librenms/build-base.php']],
+      require => File["${install_dir}/config.php"],
+    }
+
     exec { '/usr/sbin/a2enmod rewrite':
       refreshonly => true,
-      subscribe   => File['/etc/apache2/sites-enabled/librenms.conf'],
-      require     => Package['apache2'],
+      require     => Exec['/usr/bin/touch /tmp/puppet_once_lock'],
     }
 
     exec { '/usr/bin/php /var/www/librenms/build-base.php':
       refreshonly => true,
-      subscribe   => File['/etc/apache2/sites-enabled/librenms.conf'],
-      require     => Package['php5-mysql']
+      require     => Exec['/usr/bin/touch /tmp/puppet_once_lock'],
     }
 
   service { 'apache2':
