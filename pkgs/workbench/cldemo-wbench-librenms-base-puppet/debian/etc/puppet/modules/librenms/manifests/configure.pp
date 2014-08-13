0acaa35d61a61fedda5d49dc73c9d5d2497426cd
Leslie Carr <geekgirl@gmail.com>
2014-08-13 13:48:56 -0700

    removing errant "["

--- pkgs/workbench/cldemo-wbench-librenms-base-puppet/debian/etc/puppet/modules/librenms/manifests/configure.pp
+++ pkgs/workbench/cldemo-wbench-librenms-base-puppet/debian/etc/puppet/modules/librenms/manifests/configure.pp
@@ -1,7 +1,7 @@
 class librenms::configure {
     exec { '/usr/bin/touch /tmp/puppet_once_lock':
       creates => '/tmp/puppet_once_lock',
-      notify  => [Exec['/usr/sbin/a2enmod rewrite'],
+      notify  => Exec['/usr/sbin/a2enmod rewrite'],
     }
 
     exec { '/usr/sbin/a2enmod rewrite':
