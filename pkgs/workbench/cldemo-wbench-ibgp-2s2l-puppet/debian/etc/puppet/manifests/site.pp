6ae60e50d7cfef028159e1b033d91a5e69726737
Matt Willsher <matt@monki.org.uk>
2014-08-13 15:19:57 +0100

    Add template, configs, 2s ibgp

--- pkgs/workbench/cldemo-wbench-ibgp-2s2l-puppet/debian/etc/puppet/manifests/site.pp
+++ pkgs/workbench/cldemo-wbench-ibgp-2s2l-puppet/debian/etc/puppet/manifests/site.pp
@@ -2,14 +2,14 @@ node 'spine1.lab.local' {
     $int_enabled = true
     $int_loopback = '10.2.1.3'
     $int_layer3 = {
-        swp1  => { 'address' => '10.1.1.2', 'netmask' => '255.255.255.252' },
-        swp2  => { 'address' => '10.1.1.6', 'netmask' => '255.255.255.252' },
-        swp3  => { 'address' => '10.1.1.10', 'netmask' => '255.255.255.252' },
-        swp4  => { 'address' => '10.1.1.14', 'netmask' => '255.255.255.252' },
-        swp17 => { 'address' => '10.1.1.50', 'netmask' => '255.255.255.252' },
-        swp18 => { 'address' => '10.1.1.54', 'netmask' => '255.255.255.252' },
-        swp19 => { 'address' => '10.1.1.58', 'netmask' => '255.255.255.252' },
-        swp20 => { 'address' => '10.1.1.62', 'netmask' => '255.255.255.252' }
+        swp1  => { 'address' => '10.1.1.2', 'netmask' => '255.255.255.252', 'cidr_netmask' => 30 },
+        swp2  => { 'address' => '10.1.1.6', 'netmask' => '255.255.255.252', 'cidr_netmask' => 30 },
+        swp3  => { 'address' => '10.1.1.10', 'netmask' => '255.255.255.252', 'cidr_netmask' => 30 },
+        swp4  => { 'address' => '10.1.1.14', 'netmask' => '255.255.255.252', 'cidr_netmask' => 30 },
+        swp17 => { 'address' => '10.1.1.50', 'netmask' => '255.255.255.252', 'cidr_netmask' => 30 },
+        swp18 => { 'address' => '10.1.1.54', 'netmask' => '255.255.255.252', 'cidr_netmask' => 30 },
+        swp19 => { 'address' => '10.1.1.58', 'netmask' => '255.255.255.252', 'cidr_netmask' => 30 },
+        swp20 => { 'address' => '10.1.1.62', 'netmask' => '255.255.255.252', 'cidr_netmask' => 30 }
     }
     $int_bridges = { }
     $bgp = {
@@ -24,14 +24,14 @@ node 'spine2.lab.local' {
     $int_enabled = true
     $int_loopback = '10.2.1.4'
     $int_layer3 = {
-        swp1 => { 'address' => '10.1.1.18', 'netmask' => '255.255.255.252' },
-        swp2 => { 'address' => '10.1.1.22', 'netmask' => '255.255.255.252' },
-        swp3 => { 'address' => '10.1.1.26', 'netmask' => '255.255.255.252' },
-        swp4 => { 'address' => '10.1.1.30', 'netmask' => '255.255.255.252' },
-        swp17 => { 'address' => '10.1.1.34', 'netmask' => '255.255.255.252' },
-        swp18 => { 'address' => '10.1.1.38', 'netmask' => '255.255.255.252' },
-        swp19 => { 'address' => '10.1.1.42', 'netmask' => '255.255.255.252' },
-        swp20 => { 'address' => '10.1.1.46', 'netmask' => '255.255.255.252' }
+        swp1 => { 'address' => '10.1.1.18', 'netmask' => '255.255.255.252', 'cidr_netmask' => 30 },
+        swp2 => { 'address' => '10.1.1.22', 'netmask' => '255.255.255.252', 'cidr_netmask' => 30 },
+        swp3 => { 'address' => '10.1.1.26', 'netmask' => '255.255.255.252', 'cidr_netmask' => 30 },
+        swp4 => { 'address' => '10.1.1.30', 'netmask' => '255.255.255.252', 'cidr_netmask' => 30 },
+        swp17 => { 'address' => '10.1.1.34', 'netmask' => '255.255.255.252', 'cidr_netmask' => 30 },
+        swp18 => { 'address' => '10.1.1.38', 'netmask' => '255.255.255.252', 'cidr_netmask' => 30 },
+        swp19 => { 'address' => '10.1.1.42', 'netmask' => '255.255.255.252', 'cidr_netmask' => 30 },
+        swp20 => { 'address' => '10.1.1.46', 'netmask' => '255.255.255.252', 'cidr_netmask' => 30 }
     }
     $int_bridges = { }
     $bgp = {
@@ -46,18 +46,18 @@ node 'leaf1.lab.local' {
     $int_enabled = true
     $int_loopback = '10.2.1.1'
     $int_layer3 = {
-        swp1  => { 'address' => '10.1.1.1', 'netmask' => '255.255.255.252' },
-        swp2  => { 'address' => '10.1.1.5', 'netmask' => '255.255.255.252' },
-        swp3  => { 'address' => '10.1.1.9', 'netmask' => '255.255.255.252' },
-        swp4  => { 'address' => '10.1.1.13', 'netmask' => '255.255.255.252' },
-        swp17 => { 'address' => '10.1.1.33', 'netmask' => '255.255.255.252' },
-        swp18 => { 'address' => '10.1.1.37', 'netmask' => '255.255.255.252' },
-        swp19 => { 'address' => '10.1.1.41', 'netmask' => '255.255.255.252' },
-        swp20 => { 'address' => '10.1.1.45', 'netmask' => '255.255.255.252' }
+        swp1  => { 'address' => '10.1.1.1', 'netmask' => '255.255.255.252', 'cidr_netmask' => 30 },
+        swp2  => { 'address' => '10.1.1.5', 'netmask' => '255.255.255.252', 'cidr_netmask' => 30 },
+        swp3  => { 'address' => '10.1.1.9', 'netmask' => '255.255.255.252', 'cidr_netmask' => 30 },
+        swp4  => { 'address' => '10.1.1.13', 'netmask' => '255.255.255.252', 'cidr_netmask' => 30 },
+        swp17 => { 'address' => '10.1.1.33', 'netmask' => '255.255.255.252', 'cidr_netmask' => 30 },
+        swp18 => { 'address' => '10.1.1.37', 'netmask' => '255.255.255.252', 'cidr_netmask' => 30 },
+        swp19 => { 'address' => '10.1.1.41', 'netmask' => '255.255.255.252', 'cidr_netmask' => 30 },
+        swp20 => { 'address' => '10.1.1.45', 'netmask' => '255.255.255.252', 'cidr_netmask' => 30 }
     }
     $int_bridges = {
-        br0 => { 'address' => '10.4.1.1', 'netmask' => '255.255.255.128', 'members' => ['swp30','swp31','swp32','swp33'] },
-        br1 => { 'address' => '10.4.1.129', 'netmask' => '255.255.255.128', 'members' => ['swp34','swp35','swp36','swp37'] }
+        br0 => { 'address' => '10.4.1.1', 'netmask' => '255.255.255.128', 'cidr_netmask' => 25 , 'members' => ['swp30','swp31','swp32','swp33'] },
+        br1 => { 'address' => '10.4.1.129', 'netmask' => '255.255.255.128', 'cidr_netmask' => 25, 'members' => ['swp34','swp35','swp36','swp37'] }
     }
     $bgp = {
         myasn => 65000,
@@ -71,18 +71,18 @@ node 'leaf2.lab.local' {
     $int_enabled = true
     $int_loopback = '10.2.1.2'
     $int_layer3 = {
-        swp1  => { 'address' => '10.1.1.17', 'netmask' => '255.255.255.252' },
-        swp2  => { 'address' => '10.1.1.21', 'netmask' => '255.255.255.252' },
-        swp3  => { 'address' => '10.1.1.25', 'netmask' => '255.255.255.252' },
-        swp4  => { 'address' => '10.1.1.29', 'netmask' => '255.255.255.252' },
-        swp17 => { 'address' => '10.1.1.49', 'netmask' => '255.255.255.252' },
-        swp18 => { 'address' => '10.1.1.53', 'netmask' => '255.255.255.252' },
-        swp19 => { 'address' => '10.1.1.57', 'netmask' => '255.255.255.252' },
-        swp20 => { 'address' => '10.1.1.61', 'netmask' => '255.255.255.252' }
+        swp1  => { 'address' => '10.1.1.17', 'netmask' => '255.255.255.252', 'cidr_netmask' => 30 },
+        swp2  => { 'address' => '10.1.1.21', 'netmask' => '255.255.255.252', 'cidr_netmask' => 30 },
+        swp3  => { 'address' => '10.1.1.25', 'netmask' => '255.255.255.252', 'cidr_netmask' => 30 },
+        swp4  => { 'address' => '10.1.1.29', 'netmask' => '255.255.255.252', 'cidr_netmask' => 30 },
+        swp17 => { 'address' => '10.1.1.49', 'netmask' => '255.255.255.252', 'cidr_netmask' => 30 },
+        swp18 => { 'address' => '10.1.1.53', 'netmask' => '255.255.255.252', 'cidr_netmask' => 30 },
+        swp19 => { 'address' => '10.1.1.57', 'netmask' => '255.255.255.252', 'cidr_netmask' => 30 },
+        swp20 => { 'address' => '10.1.1.61', 'netmask' => '255.255.255.252', 'cidr_netmask' => 30 }
     }
     $int_bridges = {
-        br0 => { 'address' => '10.4.2.1', 'netmask' => '255.255.255.128', 'members' => ['swp30','swp31','swp32','swp33'] },
-        br1 => { 'address' => '10.4.2.129', 'netmask' => '255.255.255.128', 'members' => ['swp34','swp35','swp36','swp37'] }
+        br0 => { 'address' => '10.4.2.1', 'netmask' => '255.255.255.128', 'cidr_netmask' => 25, 'members' => ['swp30','swp31','swp32','swp33'] },
+        br1 => { 'address' => '10.4.2.129', 'netmask' => '255.255.255.128', 'cidr_netmask' => 25, 'members' => ['swp34','swp35','swp36','swp37'] }
     }
     $bgp = {
         myasn => 65000,
