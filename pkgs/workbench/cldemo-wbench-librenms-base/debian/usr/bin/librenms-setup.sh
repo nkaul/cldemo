66d84847edce72eee3345d7685c39971040f4976
Leslie Carr <geekgirl@gmail.com>
2014-08-13 14:36:32 -0700

    need to cd to librenms directory to prevent php errors

--- pkgs/workbench/cldemo-wbench-librenms-base/debian/usr/bin/librenms-setup.sh
+++ pkgs/workbench/cldemo-wbench-librenms-base/debian/usr/bin/librenms-setup.sh
@@ -2,17 +2,16 @@
 set -e
 
 # check to see if root
-if [ "$(whoami)" != root]; then
+if [ "$(whoami)" != "root" ]; then
 	printf "Please run this with sudo or as root"
 	exit 1
 fi
 
 printf "This may take a while, please be patient\n"
-# sleep to allow puppet run to create librenms user and group
-# doing this in a script instead of puppet as large puppet run 
-# seems to encourage build-base.php to fail
+
 /usr/bin/puppet agent --test
-chown -R librenms:www-data /var/www/librenms
+chown -R www-data:librenms /var/www/librenms
 printf "Building librenms database now\n"
+cd /var/www/librenms
 /usr/bin/php /var/www/librenms/build-base.php
 exit 0
